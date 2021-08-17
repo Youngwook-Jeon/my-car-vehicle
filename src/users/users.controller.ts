0d12ff5f35839a64e5d1642dcae4fede6704002a
Young wook Jeon <ela87@naver.com>
2021-08-18 00:40:29 +0900

    Serialze in the interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,11 @@
 import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
